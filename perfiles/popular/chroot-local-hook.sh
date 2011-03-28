6e722dc24f3f47a9d893ce08c0c95b2e1fbb0b1c
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2011-03-28 02:54:55 -0430

    Agregando apt-get clean y apt-get autoclean al script chroot-local-hook.sh

--- perfiles/popular/chroot-local-hook.sh
+++ perfiles/popular/chroot-local-hook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+apt-get clean
+apt-get autoclean
 
 [ -e /var/cache/apt/pkgcache.bin ] && rm -rf /var/cache/apt/pkgcache.bin
 [ -e /var/cache/apt/srcpkgcache.bin ] && rm -rf /var/cache/apt/srcpkgcache.bin
