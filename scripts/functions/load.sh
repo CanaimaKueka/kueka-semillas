8e0a6f56d669493c12cd92ecdd62d664e0d8e3c9
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2012-04-10 17:42:07 -0430

    Mejorando funciones.

---
+++ scripts/functions/load.sh
@@ -0,0 +1,266 @@
+#!/bin/sh -e
+
+CS_LOAD_PROFILE() {
+
+	SABOR="${1}"
+	shift || true
+
+	PCONFFILE="${1}"
+	shift || true
+
+	if [ -z "${PCONFFILE}" ]; then
+		ERRORMSG "La función '%s' necesita un nombre de perfil válido como argumento." "${FUNCNAME}"
+		exit 1
+	fi
+
+	if [ -e "${PCONFFILE}" ]; then
+		. "${PCONFFILE}"	
+	else
+		ERRORMSG "El archivo de configuraciones '%s' no existe." "${PCONFFILE}"
+		exit 1
+	fi
+
+	CONFIGMSG "Leyendo estado del nombre del autor" "AUTHOR_NAME"
+	if [ -z "${AUTHOR_NAME}" ] || [ "${AUTHOR_NAME}" = "none" ]; then
+		AUTHOR_NAME="${CS_NAME}"
+		WARNINGMSG "No se ha especificado el nombre del autor para el sabor en construcción."
+	fi
+	INFOMSG "Seleccionando '%s' para identificar al autor del sabor." "${AUTHOR_NAME}"
+	DEBUGMSG "AUTHOR_NAME"
+
+	CONFIGMSG "Leyendo estado del correo electrónico del autor" "AUTHOR_EMAIL"
+	if [ -z "${AUTHOR_EMAIL}" ] || [ "${AUTHOR_EMAIL}" = "none" ]; then
+		AUTHOR_EMAIL="desarrolladores@canaima.softwarelibre.gob.ve"
+		WARNINGMSG "No se ha especificado un correo para el autor del sabor en construcción."
+	fi
+	INFOMSG "Seleccionando '%s' como correo para el autor del sabor." "${AUTHOR_EMAIL}"
+	DEBUGMSG "AUTHOR_EMAIL"
+
+	CONFIGMSG "Leyendo estado de la dirección web del autor" "AUTHOR_URL"
+	if [ -z "${AUTHOR_URL}" ] || [ "${AUTHOR_URL}" = "none" ]; then
+		AUTHOR_URL="http://canaima.softwarelibre.gob.ve/"
+		WARNINGMSG "No se ha especificado una dirección web para el autor del sabor en construcción."
+	fi
+	INFOMSG "Seleccionando '%s' como dirección web para el autor del sabor." "${AUTHOR_URL}"
+	DEBUGMSG "AUTHOR_URL"
+
+	CONFIGMSG "Leyendo estado del lenguaje de sistema" "OS_LOCALE"
+	if [ -z "${OS_LOCALE}" ] || [ "${OS_LOCALE}" = "none" ]; then
+		OS_LOCALE="${LC_ALL}"
+		WARNINGMSG "No se ha definido un lenguaje de sistema para el sabor en construcción."
+	fi
+	if [ $( cat "${SUPPORTED_LOCALES}" | grep -wc "${OS_LOCALE}" ) = 0 ]; then
+		ERRORMSG "El lenguaje de sistema '%s' no está soportado por %s." "${OS_LOCALE}" "${CS_NAME}"
+		exit 1
+	fi
+	INFOMSG "Seleccionando '%s' como lenguaje de sistema para el sabor en construcción." "${OS_LOCALE}"
+	DEBUGMSG "OS_LOCALE"
+
+	CONFIGMSG "Leyendo estado de la Metadistribución base" "META_DISTRO"
+	case "$( echo ${META_DISTRO} | tr '[:upper:]' '[:lower:]' )" in
+		debian)		META_MODE="debian";;
+		ubuntu)		META_MODE="ubuntu";;
+		canaima)	META_MODE="debian";;
+		*)
+			ERRORMSG "Metadistribución '%s' no soportada por %s. Abortando." "${META_DISTRO}" "${CS_NAME}"
+			exit 1
+		;;
+	esac
+	INFOMSG "Seleccionando Metadistribución base '%s' para la construcción del sistema." "${META_DISTRO}"
+	DEBUGMSG "META_DISTRO"
+
+	CONFIGMSG "Leyendo estado de la versión para la Metadistribución base" "META_CODENAME"
+	if [ -z "${META_CODENAME}" ] || [ "${META_CODENAME}" = "none" ]; then
+		eval "META_CODENAME=\${$( echo ${META_DISTRO} | tr '[:lower:]' '[:upper:]' )_DEFAULT_CODENAME}"
+		WARNINGMSG "No se ha especificado una versión para la Metadistribución base."
+	fi
+	INFOMSG "Seleccionando '%s' como versión de la Metadistribución base." "${META_CODENAME}"
+	DEBUGMSG "META_CODENAME"
+
+	CONFIGMSG "Leyendo estado del repositorio para la Metadistribución base" "META_REPO"
+	if [ -z "${META_REPO}" ] || [ "${META_REPO}" = "none" ]; then
+		eval "META_REPO=\${$( echo ${META_DISTRO} | tr '[:lower:]' '[:upper:]' )_DEFAULT_REPO}"
+		WARNINGMSG "No se ha especificado un repositorio para la Metadistribución base."
+	fi
+	INFOMSG "Seleccionando '%s' como repositorio para la Metadistribución base." "${META_REPO}"
+	DEBUGMSG "META_REPO"
+
+	CONFIGMSG "Leyendo estado de las secciones para el repositorio de la Metadistribución base" "META_REPOSECTIONS"
+	if [ -z "${META_REPOSECTIONS}" ] || [ "${META_REPOSECTIONS}" = "none" ]; then
+		eval "META_REPOSECTIONS=\${$( echo ${META_DISTRO} | tr '[:lower:]' '[:upper:]' )_DEFAULT_REPOSECTIONS}"
+		WARNINGMSG "No se han especificado las secciones para el repositorio de la Metadistribución base."
+	fi
+	INFOMSG "Seleccionando '%s' como secciones para el repositorio de la Metadistribución base." "${META_REPOSECTIONS}"
+	DEBUGMSG "META_REPOSECTIONS"
+
+	CONFIGMSG "Leyendo estado de la lista de paquetes de sistema" "OS_PACKAGES"
+	if [ -z "${OS_PACKAGES}" ] || [ "${OS_PACKAGES}" = "none" ]; then
+		OS_PACKAGES="gnome xorg"
+		WARNINGMSG "No se han incluído elementos dentro de la lista de paquetes."
+	fi
+	INFOMSG "Seleccionando paquetes '%s' para construir el sabor."
+	DEBUGMSG "OS_PACKAGES"
+
+	CONFIGMSG "Leyendo estado de la lista de paquetes a incluir en el repositorio interno de la imagen" "OS_PACKAGES"
+	if [ -z "${IMG_POOL_PACKAGES}" ] || [ "${IMG_POOL_PACKAGES}" = "none" ]; then
+		IMG_POOL_PACKAGES="grub grub-pc"
+		WARNINGMSG "No se han incluído elementos dentro de la lista de paquetes."
+	fi
+	INFOMSG "Seleccionando paquetes '%s' para incluir en el repositorio interno."
+	DEBUGMSG "IMG_POOL_PACKAGES"
+
+	CONFIGMSG "Leyendo estado de la inclusión de archivos en el sistema" "OS_INCLUDES"
+	if [ -z "${OS_INCLUDES}" ] || [ "${OS_INCLUDES}" = "profile" ]; then
+		OS_INCLUDES="${PROFILES}${SABOR}/OS_INCLUDES/"
+		WARNINGMSG "No se han definido archivos para incluir en el sistema."
+	fi
+	if [ -d "${OS_INCLUDES}" ] && [ $( ls -1 "${OS_INCLUDES}" 2>/dev/null | wc -l ) >= 1 ]; then
+		INFOMSG "Se incluirán en el sistema los archivos presentes en el directorio '%s'." "${OS_INCLUDES}"
+	else
+		INFOMSG "'%s' está vacío o no es un directorio. Ningún archivo se incluirá en el sistema." "${OS_INCLUDES}"
+		OS_INCLUDES="none"
+	fi
+	DEBUGMSG "OS_INCLUDES"
+
+	CONFIGMSG "Leyendo estado de la ejecución de ganchos en el sistema" "OS_HOOKS"
+	if [ -z "${OS_HOOKS}" ] || [ "${OS_HOOKS}" = "profile" ]; then
+		OS_HOOKS="${PROFILES}${SABOR}/OS_HOOKS/"
+		WARNINGMSG "No se han definido ganchos para ejecutar en el sistema."
+	fi
+	if [ -d "${OS_HOOKS}" ] && [ $( ls -1 "${OS_HOOKS}" 2>/dev/null | wc -l ) >= 1 ]; then
+		INFOMSG "Se ejecutarán en el sistema los ganchos presentes en el directorio '%s'." "${OS_HOOKS}"
+	else
+		INFOMSG "'%s' está vacío o no es un directorio. Ningún gancho se ejecutará en el sistema." "${OS_HOOKS}"
+		OS_HOOKS="none"
+	fi
+	DEBUGMSG "OS_HOOKS"
+
+	CONFIGMSG "Leyendo estado de la inclusión de archivos en la imagen" "IMG_INCLUDES"
+	if [ -z "${IMG_INCLUDES}" ] || [ "${IMG_INCLUDES}" = "profile" ]; then
+		IMG_INCLUDES="${PROFILES}${SABOR}/IMG_INCLUDES/"
+		WARNINGMSG "No se han definido archivos para incluir en la imagen."
+	fi
+	if [ -d "${IMG_INCLUDES}" ] && [ $( ls -1 "${IMG_INCLUDES}" 2>/dev/null | wc -l ) >= 1 ]; then
+		INFOMSG "Se incluirán en la imagen los archivos presentes en el directorio '%s'." "${IMG_INCLUDES}"
+	else
+		INFOMSG "'%s' está vacío o no es un directorio. Ningún archivo se incluirá en la imagen." "${IMG_INCLUDES}"
+		IMG_INCLUDES="none"
+	fi
+	DEBUGMSG "IMG_INCLUDES"
+
+	CONFIGMSG "Leyendo estado de la ejecución de ganchos en la imagen" "IMG_HOOKS"
+	if [ -z "${IMG_HOOKS}" ] || [ "${IMG_HOOKS}" = "profile" ]; then
+		IMG_HOOKS="${PROFILES}${SABOR}/IMG_HOOKS/"
+		WARNINGMSG "No se han definido ganchos para incluir en la imagen."
+	fi
+	if [ -d "${IMG_HOOKS}" ] && [ $( ls -1 "${IMG_HOOKS}" 2>/dev/null | wc -l ) >= 1 ]; then
+		INFOMSG "Se incluirán en la imagen los ganchos presentes en el directorio '%s'." "${IMG_HOOKS}"
+	else
+		INFOMSG "'%s' está vacío o no es un directorio. Ningún gancho se ejecutará en la imagen." "${IMG_HOOKS}"
+		IMG_HOOKS="none"
+	fi
+	DEBUGMSG "IMG_HOOKS"
+
+	CONFIGMSG "Leyendo estado de la imagen para la portada de arranque" "IMG_SYSLINUX_SPLASH"
+	if [ -z "${IMG_SYSLINUX_SPLASH}" ] || [ "${IMG_SYSLINUX_SPLASH}" = "profile" ]; then
+		IMG_SYSLINUX_SPLASH="${PROFILES}${SABOR}/syslinux.png"
+		WARNINGMSG "No se ha definido una imagen para la portada de arranque."
+	fi
+	if [ -f "${IMG_SYSLINUX_SPLASH}" ]; then
+		if IMG_VALIDATOR "${IMG_SYSLINUX_SPLASH}" "640" "480" "image/png"; then
+			INFOMSG "Seleccionando la imagen para la portada de arranque presente en el perfil."
+		else
+			ERRORMSG "La imagen seleccionada para la portada de arranque es inapropiada."
+			ERRORMSG "Debe utilizar una imagen PNG de dimensiones menores o iguales a 640x480px."
+			exit 1
+		fi
+	elif [ -f "${TEMPLATES}profile/${META_DISTRO}/default/syslinux.png" ]
+		IMG_SYSLINUX_SPLASH="${TEMPLATES}profile/${META_DISTRO}/default/syslinux.png"
+		INFOMSG "Seleccionando la imagen predeterminada para la portada de arranque." "${IMG_SYSLINUX_SPLASH}"
+	else
+		ERRORMSG "Resultó imposible seleccionar una imagen para la portada de arranque."
+		ERRORMSG "La instalación de %s puede estar corrupta, por favor reinstala." "${CS_NAME}"
+		exit 1
+	fi	
+	DEBUGMSG "IMG_SYSLINUX_SPLASH"
+
+	CONFIGMSG "Leyendo estado del instalador nativo" "IMG_DEBIAN_INSTALLER"
+	if [ "${IMG_DEBIAN_INSTALLER}" = "true" ]; then
+		INFOMSG "Se incluirá el instalador en la imagen."
+		IMG_DEBIAN_INSTALLER="live"
+		CS_BOOTAPPEND_INSTALL="locale=${OS_LOCALE}"
+
+		CONFIGMSG "Leyendo estado de la imagen de encabezado para el instalador" "IMG_DEBIAN_INSTALLER_BANNER"
+		if [ -z "${IMG_DEBIAN_INSTALLER_BANNER}" ] || [ "${IMG_DEBIAN_INSTALLER_BANNER}" = "profile" ]; then
+			IMG_DEBIAN_INSTALLER_BANNER="${PROFILES}${SABOR}/DEBIAN_INSTALLER/banner.png"
+			WARNINGMSG "No se ha definido una imagen de encabezado para el instalador."
+		fi
+		if [ -f "${IMG_DEBIAN_INSTALLER_BANNER}" ]; then
+			if IMG_VALIDATOR "${IMG_DEBIAN_INSTALLER_BANNER}" "800" "75" "image/png"; then
+				INFOMSG "Seleccionando la imagen de encabezado presente en el perfil."
+			else
+				ERRORMSG "La imagen de encabezado seleccionada es inapropiada."
+				ERRORMSG "Debe utilizar una imagen PNG de dimensiones menores o iguales a 800x75px."
+				exit 1
+			fi
+		elif [ -f "${TEMPLATES}profile/${META_DISTRO}/default/DEBIAN_INSTALLER/banner.png" ]; then
+			IMG_DEBIAN_INSTALLER_BANNER="${TEMPLATES}profile/${META_DISTRO}/default/DEBIAN_INSTALLER/banner.png"
+		|	INFOMSG "Seleccionando imagen de encabezado predeterminada."
+		else
+			ERRORMSG "Resultó imposible seleccionar una imagen de encabezado."
+			ERRORMSG "La instalación de %s puede estar corrupta, por favor reinstala." "${CS_NAME}"
+			exit 1
+		fi
+		DEBUGMSG "IMG_DEBIAN_INSTALLER_BANNER"
+
+		CONFIGMSG "Leyendo estado de la preconfiguración para el instalador" "IMG_DEBIAN_INSTALLER_PRESEED"
+		if [ -z "${IMG_DEBIAN_INSTALLER_PRESEED}" ] || [ "${IMG_DEBIAN_INSTALLER_PRESEED}" = "profile" ]; then
+			IMG_DEBIAN_INSTALLER_PRESEED="${PROFILES}${SABOR}/DEBIAN_INSTALLER/preseed.cfg"
+			WARNINGMSG "No se ha definido una preconfiguración para el instalador."
+		fi
+		if [ -f "${IMG_DEBIAN_INSTALLER_PRESEED}" ]; then
+			INFOMSG "Seleccionando preconfiguración para el instalador presente en el perfil"
+		elif [ -f "${TEMPLATES}profile/${META_DISTRO}/default/DEBIAN_INSTALLER/preseed.cfg" ]; then
+			IMG_DEBIAN_INSTALLER_PRESEED="${TEMPLATES}profile/${META_DISTRO}/default/DEBIAN_INSTALLER/preseed.cfg"
+			INFOMSG "Seleccionando los valores predeterminados de preconfiguración para el instalador."
+		else
+			ERRORMSG "Resultó imposible seleccionar una preconfiguración para el instalador."
+			ERRORMSG "La instalación de %s puede estar corrupta, por favor reinstala." "${CS_NAME}"
+			exit 1
+		fi
+		DEBUGMSG "IMG_DEBIAN_INSTALLER_PRESEED"
+
+		CONFIGMSG "Leyendo estado del estilo visual para el instalador" "IMG_DEBIAN_INSTALLER_GTK"
+		if [ -z "${IMG_DEBIAN_INSTALLER_GTK}" ] || [ "${IMG_DEBIAN_INSTALLER_GTK}" = "profile" ]; then
+			IMG_DEBIAN_INSTALLER_GTK="${PROFILES}${SABOR}/DEBIAN_INSTALLER/gtkrc"
+			WARNINGMSG "No se ha definido un estilo visual para el instalador."
+		fi
+		if [ -f "${IMG_DEBIAN_INSTALLER_GTK}" ]; then
+			INFOMSG "Seleccionando estilo visual para el instalador presente en el perfil"
+		elif [ -f "${TEMPLATES}profile/${META_DISTRO}/default/DEBIAN_INSTALLER/gtkrc" ]; then
+			IMG_DEBIAN_INSTALLER_GTK="${TEMPLATES}profile/${META_DISTRO}/default/DEBIAN_INSTALLER/gtkrc"
+			INFOMSG "Seleccionando estilo visual predeterminado para el instalador."
+		else
+			ERRORMSG "Resultó imposible seleccionar un estilo visual para el instalador."
+			ERRORMSG "La instalación de %s puede estar corrupta, por favor reinstala." "${CS_NAME}"
+			exit 1
+		fi
+		DEBUGMSG "IMG_DEBIAN_INSTALLER_GTK"
+	else
+		IMG_DEBIAN_INSTALLER="false"
+		INFOMSG "Se incluirá el instalador en la imagen."
+	fi
+
+	OS_LANG="$( echo "${OS_LOCALE}" | sed 's/_.*//g' )"
+	CS_ISO_PREPARER="${CS_ISO_PREPARER:-${CS_NAME}; http://code.google.com/p/canaima-semilla/}"
+	CS_ISO_VOLUME="${CS_ISO_VOLUME:-${META_DISTRO}-${SABOR} (${DATE})}"	
+	CS_ISO_PUBLISHER="${CS_ISO_PUBLISHER:-${AUTHOR_NAME}; ${AUTHOR_EMAIL}; ${AUTHOR_URL}}"	
+	CS_ISO_APPLICATION="${CS_ISO_APPLICATION:-${META_DISTRO} Live}"
+	CS_LIVECONFIG_VARS=""
+	CS_BOOTAPPEND_LIVE="locale=${OS_LOCALE} keyb=${OS_LANG} quiet splash vga=791 live-config.user-fullname=${META_DISTRO}"
+cat >  << EOF
+
+EOF
+
+}
+
--- scripts/modules/build.sh
+++ scripts/modules/build.sh
@@ -40,14 +40,14 @@ elif [ "${ACTION}" == "build" ]; then
 	SHORTOPTS="f:a:m:s:inbcd"
 	LONGOPTS="config-file:,architecture:,image:,profile:,installer,no-installer,build-only,config-only,debug"
 else
-	ERROR "Error interno"
+	ERRORMSG "Error interno"
 	exit 1
 fi
 
 OPTIONS="$( getopt --shell="sh" --name="${0}" --options="${SHORTOPTS}" --longoptions="${LONGOPTS}" -- "${@}" )"
 
 if [ $? != 0 ]; then
-	ERROR "Ocurrió un problema interpretando los parámetros."
+	ERRORMSG "Ocurrió un problema interpretando los parámetros."
 	exit 1
 fi
 
@@ -118,20 +118,18 @@ while true; do
 		;;
 
                 *)
-			ERROR "Ocurrió un problema interpretando los parámetros."
+			ERRORMSG "Ocurrió un problema interpretando los parámetros."
 			exit 1
 		;;
 	esac
 done
 
-if [ -n "${BUILDDIR}" ]; then
-	if [ -e "${BUILDDIR}" ]; then
-		ISOS="${BUILDDIR}"
-		MENSAJE "Utilizando %s para construir la imagen." "${BUILDDIR}"
-	else
-		ERROR "El directorio '%s' establecido a través de la opción --dir-construir no existe." "${BUILDDIR}"
-		exit 1
-	fi
+if [ -n "${BUILDDIR}" ] && [ -e "${BUILDDIR}" ]; then
+	ISOS="${BUILDDIR}"
+	INFOMSG "Utilizando %s para construir la imagen." "${BUILDDIR}"
+else
+	ERRORMSG "El directorio '%s' establecido a través de la opción --dir-construir no existe." "${BUILDDIR}"
+	exit 1
 fi
 
 if ${CONFIGONLY} || ${VARDUMP}; then
@@ -140,70 +138,53 @@ if ${CONFIGONLY} || ${VARDUMP}; then
 
 	if [ -z "${SABOR}" ]; then
 		SABOR="popular"
-		MENSAJE "No especificaste un sabor, utilizando sabor '%s' por defecto." "${SABOR}"
+		INFOMSG "No especificaste un sabor, utilizando sabor '%s' por defecto." "${SABOR}"
 	fi
 
 	if [ -z "${ARCH}" ]; then
 		ARCH="$( dpkg --print-architecture )"
-		MENSAJE "No especificaste una arquitectura, utilizando '%s' presente en el sistema." "${ARCH}"
+		INFOMSG "No especificaste una arquitectura, utilizando '%s' presente en el sistema." "${ARCH}"
 	fi
 
 	if [ -z "${MEDIO}" ]; then
 		MEDIO="iso-hybrid"
-		MENSAJE "No especificaste un tipo de formato para la imagen, utilizando medio '%s' por defecto." "${MEDIO}"
+		INFOMSG "No especificaste un tipo de formato para la imagen, utilizando medio '%s' por defecto." "${MEDIO}"
 	fi
 
 	PCONF="${PCONF:-${ISOS}config}"
 	PHISTORY="${PHISTORY:-${ISOS}history}"
 	PCONFBKP="${PCONFBKP:-${ISOS}history/config.${DATE}.backup}"
 	PCONFFILE="${PCONFFILE:-${PROFILES}${SABOR}/profile.conf}"
-	PCONFIGDATA="${PCONFIGURED:-${ISOS}config/c-s/config-data.conf}"
-	PBUILDDATA="${PCONFIGURED:-${ISOS}config/c-s/build-data.conf}"
+	PCONFIGURED="${PCONFIGURED:-${ISOS}config/c-s/build-data.conf}"
 
 	if [ -d "${PROFILES}${SABOR}" ]; then
 		if [ -f "${PCONFFILE}" ]; then
 			CS_LOAD_PROFILE "${SABOR}" "${PCONFFILE}"
 		else
-			ERROR "El perfil '%s' no posee configuración en '%s'" "${SABOR}" "${PCONFFILE}"
+			ERRORMSG "El perfil '%s' no posee configuración en '%s'" "${SABOR}" "${PCONFFILE}"
 			exit 1
 		fi
 	else
-		ERROR "El perfil '%s' no existe dentro de la carpeta de perfiles '%s'." "${SABOR}" "${PROFILES}"
+		ERRORMSG "El perfil '%s' no existe dentro de la carpeta de perfiles '%s'." "${SABOR}" "${PROFILES}"
 		exit 1
 	fi
-
-	if [ -d "${PCONF}" ]; then
-		MENSAJE "Respaldando árbol de configuraciones previo en '%s'." "${PCONFBKP}"
-		mkdir -p "${PHISTORY}"
-		mv "${PCONF}" "${PCONFBKP}"
-	fi
-
-	cd "${ISOS}"
-
-	ADVERTENCIA "Limpiando residuos de construcciones anteriores ..."
-	rm -rf 	${ISOS}.stage \
-		${ISOS}auto \
-		${ISOS}binary.log \
-		${ISOS}cache/stages_bootstrap
-
-	lb clean
 
 	# ARQUITECTURA
 	case ${ARCH} in
 		amd64)
 			ARCH="amd64"
 			KERNEL_ARCH="amd64"
-			EXITO "Arquitectura: amd64"
+			SUCCESSMSG "Arquitectura: amd64"
 		;;
 
 		i386)
 			ARCH="i386"
 			KERNEL_ARCH="686"
-			EXITO "Arquitectura: i386"
+			SUCCESSMSG "Arquitectura: i386"
 		;;
 
 		*)
-			ERROR "Arquitectura '%s' no soportada por %s. Abortando." "${ARCH}" "${CS_NAME}"
+			ERRORMSG "Arquitectura '%s' no soportada por %s. Abortando." "${ARCH}" "${CS_NAME}"
 			exit 1
 		;;
 	esac
@@ -218,155 +199,47 @@ if ${CONFIGONLY} || ${VARDUMP}; then
 			fi
 			MEDIO_LBNAME="binary.img"
 			MEDIO_CSNAME="${META_DISTRO}-${SABOR}~${DATE}_${ARCH}.img"
-			EXITO "Medio: Imagen para dispositivos de almacenamiento extraíble (USB)."
+			SUCCESSMSG "Medio: Imagen para dispositivos de almacenamiento extraíble (USB)."
 		;;
 
 		iso|ISO|CD|DVD)
 			MEDIO="iso"
 			MEDIO_LBNAME="binary.iso"
 			MEDIO_CSNAME="${META_DISTRO}-${SABOR}~${DATE}_${ARCH}.iso"
-			EXITO "Medio: Imagen para dispositivos ópticos de almacenamiento (CD/DVD)."
+			SUCCESSMSG "Medio: Imagen para dispositivos ópticos de almacenamiento (CD/DVD)."
 		;;
 
 		iso-hybrid|hibrido|mixto|hybrid)
 			MEDIO="iso-hybrid"
 			MEDIO_LBNAME="binary-hybrid.iso"
 			MEDIO_CSNAME="${META_DISTRO}-${SABOR}~${DATE}_${ARCH}.iso"
-			EXITO "Medio: Imagen mixta para dispositivos de almacenamiento (CD/DVD/USB)."
+			SUCCESSMSG "Medio: Imagen mixta para dispositivos de almacenamiento (CD/DVD/USB)."
 		;;
 
 		*)
-			ERROR "Tipo de formato '%s' no reconocido por %s. Abortando." "${MEDIO}" "${CS_NAME}"
+			ERRORMSG "Tipo de formato '%s' no reconocido por %s. Abortando." "${MEDIO}" "${CS_NAME}"
 			exit 1
 		;;
 	esac
 
 	if [ -d "${PROFILES}${SABOR}" ]; then
 		if [ -f "${PCONFFILE}" ]; then
-			if [ -f "${EXTRACONF}" ]; then
-				. "${EXTRACONF}"
-			fi
-
-			CS_CONFIG_PROFILE "${SABOR}"
+			CS_CONFIG_LB "${SABOR}" "${PCONFIG}"
 		else
-			ERROR "El perfil '%s' no posee configuración en '%s'" "${SABOR}" "${PCONFFILE}"
+			ERRORMSG "El perfil '%s' no posee configuración en '%s'" "${SABOR}" "${PCONFFILE}"
 			exit 1
 		fi
 	else
-		ERROR "El perfil '%s' no existe dentro de la carpeta de perfiles '%s'." "${SABOR}" "${PCONFFILE}"
+		ERRORMSG "El perfil '%s' no existe dentro de la carpeta de perfiles '%s'." "${SABOR}" "${PCONFFILE}"
 		exit 1
 	fi
 
 	if [ -f "${PCONFIGURED}" ]; then
-		EXITO "Sabor: %s" "${SABOR}"
+		SUCCESSMSG "Sabor: %s" "${SABOR}"
 	else
-		ERROR "El perfil '%s' no logró configurarse correctamente. Abortando." "${SABOR}"
+		ERRORMSG "El perfil '%s' no logró configurarse correctamente. Abortando." "${SABOR}"
 		exit 1
 	fi
-
-	ADVERTENCIA "Generando árbol de configuraciones ..."
-	cd "${ISOS}"
-
-	if dpkg --compare-versions "${LB_VERSION}" ge 3.0; then
-		lb config \
-		--architecture="${ARCH}" \
-		--linux-flavours="${KERNEL_ARCH}" \
-		--distribution="${META_CODENAME}" \
-		--mode="${META_MODE}" \
-		--language="${OS_LANG}" \
-		--apt="aptitude" \
-		--apt-recommends="false" \
-		--apt-indices="none" \
-		--apt-secure="false" \
-		--bootloader="syslinux" \
-		--binary-images="${MEDIO}" \
-		--bootstrap="debootstrap" \
-		--includes="none" \
-		--hostname="${META_DISTRO}-${SABOR}" \
-		--username="${META_DISTRO}" \
-		--archive-areas="${META_REPOSECTIONS}"
-		--parent-mirror-bootstrap="${META_REPO}" \
-		--parent-mirror-chroot="${META_REPO}" \
-		--parent-mirror-binary="${META_REPO}" \
-		--parent-mirror-debian-installer="${META_REPO}" \
-		--mirror-bootstrap="${META_REPO}" \
-		--mirror-chroot="${META_REPO}" \
-		--mirror-binary="${META_REPO}" \
-		--mirror-debian-installer="${META_REPO}" \
-		--parent-mirror-chroot-security="none" \
-		--parent-mirror-chroot-volatile="none" \
-		--parent-mirror-chroot-backports="none" \
-		--parent-mirror-binary-security="none" \
-		--parent-mirror-binary-volatile="none" \
-		--parent-mirror-binary-backports="none" \
-		--mirror-chroot-security="none" \
-		--mirror-chroot-volatile="none" \
-		--mirror-chroot-backports="none" \
-		--mirror-binary-security="none" \
-		--mirror-binary-volatile="none" \
-		--mirror-binary-backports="none" \
-		--security="false" \
-		--volatile="false" \
-		--backports="false" \
-		--source="false" \
-		--iso-preparer="${CS_ISO_PREPARER}" \
-		--iso-volume="${CS_ISO_VOLUME}" \
-		--iso-publisher="${CS_ISO_PUBLISHER}" \
-		--iso-application="${CS_ISO_APPLICATION}" \
-		--memtest="none" \
-		--debian-installer="${IMG_DEBIAN_INSTALLER}" \
-		--win32-loader="false" \
-		--bootappend-live="${CS_BOOTAPPEND_LIVE}" \
-		--bootappend-install="${CS_BOOTAPPEND_INSTALL}"
-		${NULL}
-	else
-		lb config \
-		--architecture="${ARCH}" \
-		--linux-flavours="${KERNEL_ARCH}" \
-		--distribution="${META_CODENAME}" \
-		--mode="${META_MODE}" \
-		--language="${OS_LANG}" \
-		--apt="aptitude" \
-		--apt-recommends="false" \
-		--apt-secure="false" \
-		--bootloader="syslinux" \
-		--syslinux-menu="true" \
-		--syslinux-timeout="5" \
-		--syslinux-splash="${IMG_SYSLINUX_SPLASH}" \
-		--binary-images="${MEDIO}" \
-		--bootstrap="debootstrap" \
-		--binary-indices="false" \
-		--includes="none" \
-		--username="${META_DISTRO}" \
-		--hostname="${META_DISTRO}-${SABOR}" \
-		--archive-areas="${META_REPOSECTIONS}"
-		--mirror-chroot="${META_REPO}" \
-		--mirror-binary="${META_REPO}" \
-		--mirror-debian-installer="${META_REPO}" \
-		--mirror-chroot-security="none" \
-		--mirror-chroot-volatile="none" \
-		--mirror-chroot-backports="none" \
-		--mirror-binary-security="none" \
-		--mirror-binary-volatile="none" \
-		--mirror-binary-backports="none" \
-		--security="false" \
-		--volatile="false" \
-		--backports="false" \
-		--source="false" \
-		--iso-preparer="${CS_ISO_PREPARER}" \
-		--iso-volume="${CS_ISO_VOLUME}" \
-		--iso-publisher="${CS_ISO_PUBLISHER}" \
-		--iso-application="${CS_ISO_APPLICATION}" \
-		--debian-installer="${IMG_DEBIAN_INSTALLER}" \
-		--win32-loader="false" \
-		--memtest="none" \
-		--bootappend-live="${CS_BOOTAPPEND_LIVE}" \
-		--bootappend-install="${CS_BOOTAPPEND_INSTALL}"
-		${NULL}
-	fi
-	
-
-
 fi
 
 if ${BUILDONLY}; then
@@ -374,8 +247,8 @@ if ${BUILDONLY}; then
 	if [ -f "${ISOS}config/c-s/build-data.conf" ]; then
 		. "${ISOS}config/c-s/build-data.conf"
 	else
-		ADVERTENCIA "El contenedor de construcción parece haber sido configurado manualmente."
-		ADVERTENCIA "Puede que algunas características de %s no estén disponibles." "${CS_NAME}"
+		WARNINGMSG "El contenedor de construcción parece haber sido configurado manualmente."
+		WARNINGMSG "Puede que algunas características de %s no estén disponibles." "${CS_NAME}"
 		
 		if 	[ -f "${ISOS}config/common" ] && \
 			[ -f "${ISOS}config/binary" ] && \
@@ -408,14 +281,14 @@ if ${BUILDONLY}; then
 				;;
 			esac
 		else
-			ERROR "%s no pudo encontrar una configuración apropiada en %s." "${CS_NAME}" "${ISOS}config"
+			ERRORMSG "%s no pudo encontrar una configuración apropiada en %s." "${CS_NAME}" "${ISOS}config"
 			exit 1
 		fi
 	fi
 
 	cd "${ISOS}"
 	echo ""
-	ADVERTENCIA "[--- INICIANDO CONSTRUCCIÓN ---]"
+	WARNINGMSG "[--- INICIANDO CONSTRUCCIÓN ---]"
 	echo ""
 	lb build 2>&1 | tee "${ISOS}${LOGFILE}"
 
@@ -424,12 +297,12 @@ if ${BUILDONLY}; then
 		PESO="$( echo "scale=2;$( stat --format=%s "${ISOS}${MEDIO_LBNAME}" )/1048576" | bc )MB"
 		mv "${ISOS}${MEDIO_LBNAME}" "${ISOS}${MEDIO_CSNAME}"
 
-		EXITO "Se ha creado una imagen %s con un peso de %s." "${MEDIO}" "${PESO}"
-		EXITO "Puedes encontrar la imagen '%s' en el directorio %s" "${MEDIO_CSNAME}" "${ISOS}"
+		SUCCESSMSG "Se ha creado una imagen %s con un peso de %s." "${MEDIO}" "${PESO}"
+		SUCCESSMSG "Puedes encontrar la imagen '%s' en el directorio %s" "${MEDIO_CSNAME}" "${ISOS}"
 		exit 0
 	else
-		ERROR "Ocurrió un error durante la generación de la imagen."
-		ERROR "Si deseas asistencia, puedes enviar un correo a %s con el contenido del archivo '%s'" "${CS_LOG_MAIL}" "${ISOS}${LOGFILE}"
+		ERRORMSG "Ocurrió un error durante la generación de la imagen."
+		ERRORMSG "Si deseas asistencia, puedes enviar un correo a %s con el contenido del archivo '%s'" "${CS_LOG_MAIL}" "${ISOS}${LOGFILE}"
 		exit 1
 	fi
 fi
